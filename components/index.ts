5adac5542f4969682d7cdc8b3987b07e50b2b713
Pedro <jpedro1000@gmail.com>
2024-03-10 23:29:37 -0300

    Final

---
+++ components/index.ts
@@ -0,0 +1,10 @@
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import CarCard from './CarCard';
+import CarDetails from './CarDetails';
+import ShowMore from './ShowMore';
+
+export { Hero, Navbar, Footer, SearchBar, CustomFilter, CarCard, CarDetails, ShowMore }
